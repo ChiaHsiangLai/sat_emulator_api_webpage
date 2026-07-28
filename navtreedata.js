055d017761dbcfa80823ca5b4eeadd580d4d5381
chiahsiang lai <chiahsiang.lai@tronfuture.com>
2026-07-28 15:23:49 +0800

    Regenerate API docs (2026-07-28T07:23:49Z)

--- navtreedata.js
+++ navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "SNS3 Impl - Model API", "index.html", [
+  [ "Satellite network emulator API", "index.html", [
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ],
       [ "Namespace Members", "namespacemembers.html", [
